92331c21cd0cc436e090b91360984be74d0f5a13
adityanz <aditya@adityadixit.com>
2020-09-22 06:10:56 +0530

    Updates

---
+++ precache-manifest.58bbfa2108db43cebd11183dc4519155.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85bcf54367ba3c5629ab1e78e8240544",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8b6db8e0579dd9a1dbe",
+    "url": "/static/css/main.483cada3.chunk.css"
+  },
+  {
+    "revision": "4e4d2046f2c90a4ccba4",
+    "url": "/static/js/2.e926dfff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e926dfff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8b6db8e0579dd9a1dbe",
+    "url": "/static/js/main.0d618fc8.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "924f78c56ed4e6a40474788fa14a5575",
+    "url": "/static/media/bar.924f78c5.csv"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
